1b95cba986df8cbca9092446f505335ec6e42236
刘成 <emitremmus0235@163.com>
2016-08-31 17:19:54 +0800

    添加网路日志打印以及自动打开预览页

--- client/server.js
+++ client/server.js
@@ -1,23 +1,32 @@
+// 借鉴了 @1340641314 的项目 react-cnode，传送门：https://github.com/1340641314/react-cnode/blob/master/server.js
+
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var opn = require('opn');
+var morgan = require('morgan');
 
 // 相当于通过本地node服务代理请求到了http://cnodejs.org/api
 var proxy = [{
-    path: '/api/*',
-    target: 'https://cnodejs.org',
-    host: 'cnodejs.org'
+  path: '/api/v1/*',
+  target: 'https://cnodejs.org',
+  host: 'cnodejs.org'
 }];
 
 //启动服务
 var server = new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    proxy:proxy
+  publicPath: config.output.publicPath,
+  proxy: proxy
 });
+
+server.app.use(morgan());
 
 //将其他路由，全部返回index.html
-server.app.get('*', function (req,res) {
-    res.sendFile(__dirname + '/index.html')
+server.app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
 });
 
-server.listen(3000);
+server.listen(5000, function() {
+  console.log('server is running at http://localhost:5000/');
+  opn('http://localhost:5000/');
+});
